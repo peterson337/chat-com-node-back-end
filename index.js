6f55f9931675c8205d8d44ed15c77ba8f1bf46f3
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-10-13 16:00:48 -0300

    Projeto finalizado

---
+++ index.js
@@ -0,0 +1,50 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { cors: { origin: "http://localhost:3000" } }); 
+const cors = require('cors');
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+    console.log('Usuário conectado');
+
+    //io.emit('conectado', 'Estou conectado!');
+
+    socket.broadcast.emit('novo usuário', 'Estou conectado!')
+
+
+    socket.on('set_username', username => {
+        socket.data.username = username;
+    })
+
+    socket.on('message', (text) => {
+
+      io.emit('receive_message', {
+        text,
+        authorId: socket.id,
+        author: socket.data.username
+      })
+      // socket.broadcast.emit('message', {
+      //   username: socket.data.username,
+      // })
+    })
+    // socket.on('disconnect', (reason) => {
+    //     console.log('Usuário desconetado');
+    // })
+    
+  });
+  app.get('/', (req, res) => {
+  
+      res.json({
+          "nome": 200,
+          "id": 0,
+          "message": "Hello World!"
+        
+        });
+  
+  
+  });
+  
+http.listen(5000, () => console.log('server in runing in 5000'));
+
+
