8bae39a38e87ffd8a07280ae3d074431c0e8bfba
Peterson Rodrigues <petersonpepi16@gmail.com>
2023-10-19 09:06:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, { cors: { origin: "https://chat-tempo-real.vercel.app/" } }); 
+const io = require('socket.io')(http, { cors: { origin: "https://chat-tempo-real.vercel.app" } }); 
 const cors = require('cors');
 
 app.use(cors());
